23240b3e8de75addbbdc0e5c27fd3fb84b312f18
Vladislav Osmianskij <vlaosm@gmail.com>
2021-03-17 10:53:53 +0200

    fix: compiler should stop on the first PHP warning

--- bin/compile.php
+++ bin/compile.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use Osm\Runtime\Apps;
+use function Osm\handle_errors;
 
 require 'vendor/autoload.php';
+handle_errors();
 
 Apps::$project_path = getcwd();
 Apps::compile(ltrim($argv[1], '\\'));
