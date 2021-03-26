414684c31a81ba66c5388247f3c0d2f7bbd0ddc1
Vladislav Osmianskij <vlaosm@gmail.com>
2021-03-26 16:16:36 +0200

    minor: `throw new NotImplemented($this)` shows actual class missing the implementation.

--- src/Exceptions/NotImplemented.php
+++ src/Exceptions/NotImplemented.php
@@ -4,7 +4,19 @@ namespace Osm\Core\Exceptions;
 
 class NotImplemented extends \Exception
 {
-    public function __construct(string $message = "", int $code = 0, \Throwable $previous = null) {
-        parent::__construct($message ?: 'Not implemented', $code, $previous);
+    public function __construct(string|object $message = "", int $code = 0,
+        \Throwable $previous = null)
+    {
+        $caller = $this->getTrace()[0];
+
+        if (is_object($message)) {
+            $class = $message::class;
+            $message = "'{$class}::{$caller['function']}()' not implemented";
+        }
+        elseif (!$message) {
+            $message = "'{$caller['class']}::{$caller['function']}()' not implemented";
+        }
+
+        parent::__construct($message, $code, $previous);
     }
 }
